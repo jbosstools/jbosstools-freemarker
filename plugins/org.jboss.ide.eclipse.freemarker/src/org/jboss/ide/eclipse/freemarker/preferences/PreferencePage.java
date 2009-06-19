4e595133dcc7f81a699d8b5b3e2c35ad038c367f
Nick Boldt <nboldt@redhat.com>
2009-06-19 20:56:50 +0000

    https://jira.jboss.org/jira/browse/JBIDE-4488 switch to "JBoss by Red Hat" and update (c) to 2009 as appropriate
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@16087 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.ide.eclipse.freemarker/src/org/jboss/ide/eclipse/freemarker/preferences/PreferencePage.java
+++ plugins/org.jboss.ide.eclipse.freemarker/src/org/jboss/ide/eclipse/freemarker/preferences/PreferencePage.java
@@ -1,6 +1,6 @@
 /*
- * JBoss, a division of Red Hat
- * Copyright 2006, Red Hat Middleware, LLC, and individual contributors as indicated
+ * JBoss by Red Hat
+ * Copyright 2006-2009, Red Hat Middleware, LLC, and individual contributors as indicated
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
  *
